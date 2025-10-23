18e36a3d00d8a18615d8e7ccbddfc1bda18285d7
Shulker-s <160034295+Shulker-s@users.noreply.github.com>
2025-10-24 00:25:35 +0800

    Delete DEFL_IVF directory

--- DEFL_IVF/include/Utils/space.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<cmath>
-// #ifdef CALC_DIST_COUNT
-extern double dcmp;
-// #endif
-
-float L2_no_sqrt(float *a, float *b, int dim);
-
-float L2_sqrt(float *a, float *b, int dim);
